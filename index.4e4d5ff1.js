bb0ceddc296e583ba9f92060cb060a876196fec7
AnastasiaBndr <125996065+AnastasiaBndr@users.noreply.github.com>
2023-08-03 16:49:53 +0000

    Deploying to gh-pages from @ AnastasiaBndr/goit-js-hw-11@b3651bd6e8c7c64fd0b20c54a186a6f166b2a129 🚀

---
+++ index.4e4d5ff1.js
@@ -0,0 +1,2 @@
+$(".search-button").click(function(){$(this).parent().toggleClass("open")});
+//# sourceMappingURL=index.4e4d5ff1.js.map
--- index.6e5d5ae0.js
+++
@@ -1,2 +0,0 @@
-$(".search-button").click((function(){$(this).parent().toggleClass("open")}));
-//# sourceMappingURL=index.6e5d5ae0.js.map
